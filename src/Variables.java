7eadef538613eca7902890c02815ad3091f4dfa6
Xaria A <xaria.shella@gmail.com>
2022-09-13 09:35:24 -0500

    Initial commit

---
+++ src/Variables.java
@@ -0,0 +1,37 @@
+public class Variables {
+
+        public static void main ( String[] args )
+        {
+            //define 1 variable of each of the
+            //following data types
+            //byte	short		int 		long
+            //float	double
+            //char      boolean	String
+
+            //integer variables
+            byte byteEx = 127;
+            short shortEx = 12557;
+            int intEx = -17254352;
+            long longEx = 1627756382871923L;
+
+            float floatEx = 1264.364F;
+            double doubleEx = 1247378.12541384;
+
+            char d = 'D';
+            boolean lightSwitch = true;
+            String h = "hi";
+
+
+
+            System.out.println("Byte-" + byteEx + "8-bits");
+            System.out.println("Short-" + shortEx + "16-bits");
+            System.out.println("Int-" + intEx + "32-bits");
+            System.out.println("Long-" + longEx + "64-bits");
+            System.out.println("Float-" + floatEx + "32-bits");
+            System.out.println("Double-" + doubleEx + "64-bits");
+            System.out.println("Boolean-" + lightSwitch + " statement");
+            System.out.println(d);
+            System.out.println("");
+
+        }
+    }
